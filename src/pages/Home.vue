de939a0b98e8e04593b90091e302d485c5136219
Bannaphon <112239420+Bannaphon@users.noreply.github.com>
2022-10-15 16:28:15 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <navbar />
+  <section class="home">
+
+    <div class="image">
+        <img src="images/day6-.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>hi, {{name}}</h3>
+        <span>{{motto}}</span>
+        <p>{{content}}</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+  data() {
+      return {
+        name: 'Day 6',
+        motto: 'เป็นวงดนตรีจากค่าย JYP Entertainment ปัจจุบันมีสมาชิกทั้งหมด 5 คน คือ เจ (🐥) , ซองจิน (🐻) , ยองเค (🦊) , วอนพิล (🐰) , โดอุน (🐶)',
+        content: 'We Are My Day',
+      }
+ }
+
+}
+</script>
+
+<style>
+
+</style>
